87505b0f318e388db1d9748d714ad3d33fc044e5
Siberiawind <siberiawind97@naver.com>
2022-03-01 12:52:06 +0800

    Added Assignment2 homework.

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -24,28 +24,12 @@ int main(int argc, char* argv[])
 
 	Eigen::Vector3f eye_pos = { 0, 0, 5 };
 
-	std::vector<Eigen::Vector3f> pos{
-		{2, 0, -2}, {0, 2, -2}, {-2, 0, -2},
-		{3.5, -1, -5}, {2.5, 1.5, -5}, {-1, 0.5, -5},
-	};
+	std::vector<Eigen::Vector3f> pos{ {2,0,-2}, {0,2,-2}, {-2, 0, -2} };
 
-	std::vector<Eigen::Vector3i> ind{
-		{0, 1, 2},
-		{3, 4, 5},
-	};
-
-	std::vector<Eigen::Vector3f> cols{
-		{217.0, 238.0, 185.0},
-		{217.0, 238.0, 185.0},
-		{217.0, 238.0, 185.0},
-		{185.0, 217.0, 238.0},
-		{185.0, 217.0, 238.0},
-		{185.0, 217.0, 238.0},
-	};
+	std::vector<Eigen::Vector3i> ind{ {0,1,2} };
 
 	auto pos_id = r.load_positions(pos);
 	auto ind_id = r.load_indices(ind);
-	auto col_id = r.load_colors(cols);
 
 	char key = 0;
 	int frame_count = 0;
@@ -58,7 +42,7 @@ int main(int argc, char* argv[])
 		r.set_view(get_view_matrix(eye_pos));
 		r.set_projection(get_projection_matrix(60, 0.1, 0.2, 50));
 
-		r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
+		r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
 		cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
 
@@ -75,9 +59,9 @@ int main(int argc, char* argv[])
 
 		r.set_model(get_model_matrix(angle));
 		r.set_view(get_view_matrix(eye_pos));
-		r.set_projection(get_projection_matrix(60, 0.1, 0.2, 50));
+		r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
-		r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
+		r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
 		cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
 
--- Assignment2/rasterizer.h
+++ Assignment2/rasterizer.h
@@ -37,11 +37,6 @@ namespace rst
 		int ind_id = 0;
 	};
 
-	struct col_buf_id
-	{
-		int col_id = 0;
-	};
-
 	class rasterizer
 	{
 	public:
@@ -49,7 +44,6 @@ namespace rst
 
 		pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
 		ind_buf_id load_indices(const std::vector<Eigen::Vector3i>& indices);
-		col_buf_id load_colors(const std::vector<Eigen::Vector3f>& colors);
 
 		void set_model(const Eigen::Matrix4f& m);
 		void set_view(const Eigen::Matrix4f& v);
@@ -59,7 +53,7 @@ namespace rst
 
 		void clear(Buffers buf);
 
-		void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, rst::col_buf_id col_buffer, Primitive type);
+		void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, Primitive type);
 
 		std::vector<Eigen::Vector3f>& frame_buffer() { return frame_buf; }
 
@@ -67,9 +61,7 @@ namespace rst
 		void draw_line(Eigen::Vector3f begin, Eigen::Vector3f end);
 		void drawDDALine(Eigen::Vector3f begin, Eigen::Vector3f end);
 		void drawBresenhamLine(Eigen::Vector3f begin, Eigen::Vector3f end);
-
 		void rasterize_wireframe(const Triangle& t);
-		void rasterize_triangle(const Triangle& t);
 
 		int get_index(int x, int y);
 
@@ -85,7 +77,6 @@ namespace rst
 
 		std::map<int, std::vector<Eigen::Vector3f> > pos_buf;
 		std::map<int, std::vector<Eigen::Vector3i> > ind_buf;
-		std::map<int, std::vector<Eigen::Vector3f> > col_buf;
 
 		std::vector<Eigen::Vector3f> frame_buf;
 
